32c21af3eec595a8b95d9b370f33de3911c0d72b
warcry98 <72479786+warcry98@users.noreply.github.com>
2023-02-01 03:24:13 +0700

    Supposed to use max instead sum (Revision after solution release)

--- Week1/SQL/4largest_trip.sql
+++ Week1/SQL/4largest_trip.sql
@@ -1,6 +1,6 @@
 SELECT
-    date_trunc('day', lpep_pickup_datetime) as pickup_datetima,
-    sum(trip_distance) as total_trip
+    date_trunc('day', lpep_pickup_datetime) as pickup_datetime,
+    max(trip_distance) as trip -- sum(trip_distance) as total_trip # supposed to use max
 FROM green_tripdata
 WHERE
     date_trunc('day', lpep_pickup_datetime) = '2019-01-18'
@@ -8,5 +8,5 @@ WHERE
     OR date_trunc('day', lpep_pickup_datetime) = '2019-01-15'
     OR date_trunc('day', lpep_pickup_datetime) = '2019-01-10'
 GROUP BY
-    date_trunc('day', lpep_pickup_datetime)
-ORDER BY total_trip DESC
+    date_trunc('day', lpep_pickup_datetime) -- ORDER BY total_trip DESC
+ORDER BY trip DESC
