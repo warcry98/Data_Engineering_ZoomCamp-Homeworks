2db3ce9f4ac4969177ddc4bbcb6c68521e2d0bdd
warcry98 <72479786+warcry98@users.noreply.github.com>
2023-02-09 10:14:27 +0700

    Add yaml deployment

--- Week2/04_05-flows/etl_web_to_gcs-prefect_cloud.py
+++ Week2/04_05-flows/etl_web_to_gcs-prefect_cloud.py
@@ -1,9 +1,11 @@
 import os
 from pathlib import Path
-from urllib.parse import urlparse
 import pandas as pd
 from prefect import flow, task
 from prefect_gcp.cloud_storage import GcsBucket
+from prefect.filesystems import GCS
+from random import randint
+
 
 PATH_DIRECTORY = Path().resolve()
 DATA_DIRECTORY = PATH_DIRECTORY / "data"
@@ -11,23 +13,25 @@ exists = os.path.exists(DATA_DIRECTORY)
 if not exists:
   os.mkdir(DATA_DIRECTORY)
 
-
 @task(retries=3)
 def fetch(dataset_url: str) -> pd.DataFrame:
     """Read taxi data from web into pandas DataFrame"""
-    parse_url = urlparse(dataset_url)
-    dataset_file = os.path.basename(parse_url.path)
-    if not os.path.isfile(f"{DATA_DIRECTORY}/{dataset_file}"):
-        os.system(f"wget {dataset_url} -P {DATA_DIRECTORY}/")
-    df = pd.read_csv(DATA_DIRECTORY/dataset_file)
+    # if randint(0, 1) > 0:
+    #     raise Exception
+
+    df = pd.read_csv(dataset_url)
     return df
 
 
 @task(log_prints=True)
 def clean(df: pd.DataFrame) -> pd.DataFrame:
     """Fix dtype issues"""
-    df["tpep_pickup_datetime"] = pd.to_datetime(df["tpep_pickup_datetime"])
-    df["tpep_dropoff_datetime"] = pd.to_datetime(df["tpep_dropoff_datetime"])
+    if "lpep_pickup_datetime" in df.columns:
+        df["lpep_pickup_datetime"] = pd.to_datetime(df["lpep_pickup_datetime"])
+        df["lpep_dropoff_datetime"] = pd.to_datetime(df["lpep_dropoff_datetime"])
+    if "tpep_pickup_datetime" in df.columns:
+        df["tpep_pickup_datetime"] = pd.to_datetime(df["tpep_pickup_datetime"])
+        df["tpep_dropoff_datetime"] = pd.to_datetime(df["tpep_dropoff_datetime"])
     print(df.head(2))
     print(f"columns: {df.dtypes}")
     print(f"rows: {len(df)}")
@@ -47,26 +51,25 @@ def write_local(df: pd.DataFrame, color: str, dataset_file: str) -> Path:
 @task()
 def write_gcs(path: Path) -> None:
     """Upload local parquet file to GCS"""
-    gcs_block = GcsBucket.load("zoom-gcs")
-    gcs_block.upload_from_path(from_path=f"{path}", to_path=f"{path}")
+    gcs_block = GCS.load("zoom-gcs")
+    gcs_block.put_directory(local_path=f"{path}", to_path=f"{path}")
     return
 
 
 @flow()
-def etl_web_to_gcs() -> None:
+def etl_web_to_gcs(year: int, month: int, color: str) -> None:
     """The main ETL function"""
-    color = "yellow"
-    year = 2019
-    months = [2, 3]
-    for month in months:
-        dataset_file = f"{color}_tripdata_{year}-{month:02}"
-        dataset_url = f"https://github.com/DataTalksClub/nyc-tlc-data/releases/download/{color}/{dataset_file}.csv.gz"
+    dataset_file = f"{color}_tripdata_{year}-{month:02}"
+    dataset_url = f"https://github.com/DataTalksClub/nyc-tlc-data/releases/download/{color}/{dataset_file}.csv.gz"
 
-        df = fetch(dataset_url)
-        df_clean = clean(df)
-        path = write_local(df_clean, color, dataset_file)
-        write_gcs(path)
+    df = fetch(dataset_url)
+    df_clean = clean(df)
+    path = write_local(df_clean, color, dataset_file)
+    write_gcs(path)
 
 
 if __name__ == "__main__":
-    etl_web_to_gcs()
+    color = "green"
+    year = 2020
+    month = 11
+    etl_web_to_gcs(year, month, color)
--- Week2/blocks/make_gcp_blocks.py
+++ Week2/blocks/make_gcp_blocks.py
@@ -4,11 +4,11 @@ from prefect_gcp.cloud_storage import GcsBucket
 credentials_block = GcpCredentials(
     service_account_info={}
 )
-credentials_block.save("zoom-gcp-creds", overwrite=True)
+credentials_block.save("zoom-gcp-creds-prefect", overwrite=True)
 
 bucket_block = GcsBucket(
     gcp_credentials=GcpCredentials.load("zoom-gcp-creds"),
     bucket="prefect-de-zoomcamp"
 )
 
-bucket_block.save("zoom-gcs", overwrite=True)
+bucket_block.save("zoom-gcs-prefect", overwrite=True)
